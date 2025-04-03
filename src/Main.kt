728b90ea44d9f5c96bb1d473ea5829592bc127f9
reawz <raicheofficial@mail.ru>
2025-04-03 13:40:07 +0300

    1 commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,7 +1,3 @@
-/**
- * Главный файл симуляции острова.
- * Отвечает за инициализацию и запуск симуляции.
- */
 package com.javarush.island
 
 import Classes.Animal
@@ -13,28 +9,17 @@ import com.javarush.island.entities.animals.herbivores.*
 import MainLogic.Simulation
 import kotlin.random.Random
 
-/**
- * Точка входа в программу.
- * Инициализирует остров, населяет его животными и растениями, запускает симуляцию.
- */
 fun main() {
     val island = createIsland()
     populateIsland(island)
     startSimulation(island)
 }
 
-/**
- * Создает новый остров с заданными размерами
- */
 private fun createIsland(): Island {
     return Island(Configuration.width, Configuration.height)
 }
 
-/**
- * Заполняет остров животными и растениями
- */
 private fun populateIsland(island: Island) {
-    // Определяем фабрики для создания животных
     val animalFactories = AnimalFactories(
         herbivores = listOf(
             { Caterpillar() },
@@ -57,25 +42,18 @@ private fun populateIsland(island: Island) {
         )
     )
 
-    // Заселяем остров животными
     populateWithAnimals(island, animalFactories)
-    
-    // Добавляем растения
+
     populateWithPlants(island)
 }
 
-/**
- * Заселяет остров животными
- */
 private fun populateWithAnimals(island: Island, factories: AnimalFactories) {
-    // Заселяем травоядных
     factories.herbivores.forEach { creator ->
         repeat(40) {
             addRandomly(island, creator())
         }
     }
 
-    // Заселяем хищников
     factories.predators.forEach { creator ->
         repeat(25) {
             addRandomly(island, creator())
@@ -83,26 +61,17 @@ private fun populateWithAnimals(island: Island, factories: AnimalFactories) {
     }
 }
 
-/**
- * Добавляет растения на остров
- */
 private fun populateWithPlants(island: Island) {
     repeat(100) {
         addRandomly(island, Plant())
     }
 }
 
-/**
- * Запускает симуляцию
- */
 private fun startSimulation(island: Island) {
     val simulation = Simulation(island, Configuration.period)
     simulation.start()
 }
 
-/**
- * Добавляет объект в случайную клетку острова
- */
 private fun addRandomly(island: Island, entity: Any) {
     val x = Random.nextInt(Configuration.width)
     val y = Random.nextInt(Configuration.height)
@@ -112,9 +81,6 @@ private fun addRandomly(island: Island, entity: Any) {
     }
 }
 
-/**
- * Класс для хранения фабрик создания животных
- */
 private data class AnimalFactories(
     val herbivores: List<() -> Animal>,
     val predators: List<() -> Animal>
